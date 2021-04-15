22523eb94d1ba80c1534f31367db5cf070db703e
Shreyas Kr <shreyaskr1022@gmail.com>
2021-04-16 00:13:17 +0530

    Updated signup, login and home pages. Pre-validation structure of signup component done.

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse, HttpParams } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  private url = 'http://localhost:3000'
+
+  constructor(
+    public http: HttpClient
+  ) { }
+
+  //get user info from local storage
+  public getUserInfoFromLocalstorage = () => {
+    return JSON.parse(localStorage.getItem('userInfo'));
+  }
+
+  //set user info on local storage 
+  public setUserInfoInLocalStorage = (data) => {
+    localStorage.setItem('userInfo', JSON.stringify(data))
+  }
+
+  public signupFunction(data): Observable<any> {
+    const params = new HttpParams()
+      .set('email', data.email)
+      .set('password', data.password)
+
+    return this.http.post(`${this.url}/api/v1/users/signup`, params);
+  } // end of signup function.
+
+  public loginFunction(data): Observable<any> {
+    const params = new HttpParams()
+      .set('email', data.email)
+      .set('password', data.password);
+
+    return this.http.post(`${this.url}/api/v1/users/login`, params);
+
+  } //end of login function
+
+  public logoutFunction(): Observable<any> {
+    const params = new HttpParams()
+      .set('authToken', Cookie.get('authToken'))
+
+    return this.http.post(`${this.url}/api/v1/users/logout`, params);
+  } //end of logout function
+
+  private handleErrorFunction(err: HttpErrorResponse) {
+
+    let errorMessage = '';
+    if (err.error instanceof Error) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    } // end of if condition
+
+    console.error(errorMessage);
+
+    return Observable.throw(errorMessage);
+
+  }  // end of handleError function
+
+
 }
+
--- src/app/user/signup/signup.component.ts
+++ src/app/user/signup/signup.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AppService } from './../../app.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  profileSignupForm = new FormGroup({
+    userMail: new FormControl(null, [Validators.required, Validators.email]),
+    userPassword: new FormControl(null, [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")]),
+    userConfirmPassword: new FormControl(null, [Validators.required, Validators.pattern("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$")])
+  });
+
+  public result: boolean = true;
+
+  public countryList: any = ["Bangladesh", "Belgium", "Burkina Faso", "Bulgaria", "Bosnia and Herzegovina", "Barbados", "Wallis and Futuna", "Saint Barthelemy", "Bermuda", "Brunei", "Bolivia", "Bahrain", "Burundi", "Benin", "Bhutan", "Jamaica", "Bouvet Island", "Botswana", "Samoa", "Bonaire, Saint Eustatius and Saba", "Brazil", "Bahamas", "Jersey", "Belarus", "Belize", "Russia", "Rwanda", "Serbia", "East Timor", "Reunion", "Turkmenistan", "Tajikistan", "Romania", "Tokelau", "Guinea-Bissau", "Guam", "Guatemala", "South Georgia and the South Sandwich Islands", "Greece", "Equatorial Guinea", "Guadeloupe", "Japan", "Guyana", "Guernsey", "French Guiana", "Georgia", "Grenada", "United Kingdom", "Gabon", "El Salvador", "Guinea", "Gambia", "Greenland", "Gibraltar", "Ghana", "Oman", "Tunisia", "Jordan", "Croatia", "Haiti", "Hungary", "Hong Kong", "Honduras", "Heard Island and McDonald Islands", "Venezuela", "Puerto Rico", "Palestinian Territory", "Palau", "Portugal", "Svalbard and Jan Mayen", "Paraguay", "Iraq", "Panama", "French Polynesia", "Papua New Guinea", "Peru", "Pakistan", "Philippines", "Pitcairn", "Poland", "Saint Pierre and Miquelon", "Zambia", "Western Sahara", "Estonia", "Egypt", "South Africa", "Ecuador", "Italy", "Vietnam", "Solomon Islands", "Ethiopia", "Somalia", "Zimbabwe", "Saudi Arabia", "Spain", "Eritrea", "Montenegro", "Moldova", "Madagascar", "Saint Martin", "Morocco", "Monaco", "Uzbekistan", "Myanmar", "Mali", "Macao", "Mongolia", "Marshall Islands", "Macedonia", "Mauritius", "Malta", "Malawi", "Maldives", "Martinique", "Northern Mariana Islands", "Montserrat", "Mauritania", "Isle of Man", "Uganda", "Tanzania", "Malaysia", "Mexico", "Israel", "France", "British Indian Ocean Territory", "Saint Helena", "Finland", "Fiji", "Falkland Islands", "Micronesia", "Faroe Islands", "Nicaragua", "Netherlands", "Norway", "Namibia", "Vanuatu", "New Caledonia", "Niger", "Norfolk Island", "Nigeria", "New Zealand", "Nepal", "Nauru", "Niue", "Cook Islands", "Kosovo", "Ivory Coast", "Switzerland", "Colombia", "China", "Cameroon", "Chile", "Cocos Islands", "Canada", "Republic of the Congo", "Central African Republic", "Democratic Republic of the Congo", "Czech Republic", "Cyprus", "Christmas Island", "Costa Rica", "Curacao", "Cape Verde", "Cuba", "Swaziland", "Syria", "Sint Maarten", "Kyrgyzstan", "Kenya", "South Sudan", "Suriname", "Kiribati", "Cambodia", "Saint Kitts and Nevis", "Comoros", "Sao Tome and Principe", "Slovakia", "South Korea", "Slovenia", "North Korea", "Kuwait", "Senegal", "San Marino", "Sierra Leone", "Seychelles", "Kazakhstan", "Cayman Islands", "Singapore", "Sweden", "Sudan", "Dominican Republic", "Dominica", "Djibouti", "Denmark", "British Virgin Islands", "Germany", "Yemen", "Algeria", "United States", "Uruguay", "Mayotte", "United States Minor Outlying Islands", "Lebanon", "Saint Lucia", "Laos", "Tuvalu", "Taiwan", "Trinidad and Tobago", "Turkey", "Sri Lanka", "Liechtenstein", "Latvia", "Tonga", "Lithuania", "Luxembourg", "Liberia", "Lesotho", "Thailand", "French Southern Territories", "Togo", "Chad", "Turks and Caicos Islands", "Libya", "Vatican", "Saint Vincent and the Grenadines", "United Arab Emirates", "Andorra", "Antigua and Barbuda", "Afghanistan", "Anguilla", "U.S. Virgin Islands", "Iceland", "Iran", "Armenia", "Albania", "Angola", "Antarctica", "American Samoa", "Argentina", "Australia", "Austria", "Aruba", "India", "Aland Islands", "Azerbaijan", "Ireland", "Indonesia", "Ukraine", "Qatar", "Mozambique"]
+
+  constructor(
+    public appService: AppService,
+    public router: Router,
+    public toastr: ToastrService,
+    vcr: ViewContainerRef
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  verify() {
+    if (this.profileSignupForm.controls.userPassword.value == this.profileSignupForm.controls.userConfirmPassword.value) {
+      this.result = true;
+    }
+    else {
+      this.result = false;
+    }
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login'])
+  }
+
+  signUpFunction(f) {
+    // TODO: Use EventEmitter with form value
+    let data = {
+      email: f.controls.userMail.value,
+      password: f.controls.userPassword.value
+    }
+    console.log(data);
+
+    this.appService.signupFunction(data).subscribe((apiResult) => {
+
+      console.log(`Response from backend: ${apiResult}`);
+
+      if (apiResult.status === 200) {
+        setTimeout(() => {
+          this.goToLogin();
+        }, 2000);
+      } else {
+        this.toastr.error(apiResult.message)
+      }
+    }, (err) => {
+      this.toastr.error("Some Error Occured");
+    })
   }
 
 }
