597527c22cea61ffb9a9e807d3bf0c3f18ff2f74
Shreyas Kr <shreyaskr1022@gmail.com>
2021-04-15 19:24:51 +0530

    Created basic folder structure for user management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { SignupComponent } from './user/signup/signup.component';
+import { HomeComponent } from './general/home/home.component';
+import { LoginComponent } from './user/login/login.component';
 import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppService } from './app.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'signup', component: SignupComponent },
+      { path: '*', component: HomeComponent },
+      { path: '**', component: HomeComponent }
+
+    ])
   ],
-  providers: [],
+  providers: [AppService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
